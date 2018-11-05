1f8b1990904e02a88542267d92a9586f89e1b9c0
Michael-S-Kelly <mike.s.kelly@icloud.com>
2018-11-05 13:24:19 -0800

    Initial commiot

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var products = []:
+
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+
+}
+
+var tracker = {
+  products: [],
+  totalClicks: 0,
+  mainEL: document.getElementById('mainStuff'),
+  instructionEl: document.createElement('h1'),
+  mainEL: 
+
+  getRandomIndex: function(){
+    
+  },
+
+  getUniqueImages: function() {
+
+  },
+
+  renderImages: function() {
+
+  },
+
+  addClickTracker: function() {
+
+  },
+
+  clickHandler: function() {
+
+  },
+};
+
+(function createProducts() {
+
+})()
