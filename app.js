47a4f39662df852643cdb7044e9799dc5b7559ef
Michael-S-Kelly <mike.s.kelly@icloud.com>
2018-11-05 16:28:22 -0800

    Good breaking point

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 'use strict';
 
-var products = []:
+var products = [],
 
 function Product(name, src) {
-  this.name = name;
-  this.src = src;
+  this.name = name; //name of product
+  this.src = src; //href of object
+  this.votesForProduct = 0; //number of votes received for product
+
+  products.push(this);
+
+
 
 }
 
+function title() {
+  var mainEL = document.getElementById('mainStuff');
+  var titleH1El = document.createElement('h1');
+  mainEL.appendChild(titleH1El);
+  titleH1El.textContent = 'Please click on your favorite product of the three choices bellow.';
+}
+
+title();
+/*
 var tracker = {
   products: [],
   totalClicks: 0,
@@ -39,3 +53,24 @@ var tracker = {
 (function createProducts() {
 
 })()
+*/
+
+new products ('R2D2 Carry-On Bag', /asset/bag.jpg);
+new products ('Banana Slicer', /asset/banana.jpg);
+new products ('Bathroom iPad Holder', /asset/bathroom.jpg);
+new products ('Boots', /asset/boots.jpg);
+new products ('Breakfast Combo Unit', /asset/breakfast.jpg);
+new products ('Meatball Bubblegum', /asset/bubblegum.jpg);
+new products ('Chair', /asset/chair.jpg);
+new products ('Cthulhu Play Figure', /asset/cthulhu.jpg);
+new products ('Duck Mask for Dogs', /asset/dog-duck.jpg);
+new products ('Can of Dragon Meat', /asset/dragon.jpg);
+new products ('Pens with Eating Utencil Caps', /asset/pen.jpg);
+new products ('Dust Mop for Pets', /asset/pet-sweep.jpg);
+new products ('Pizza Scissors', /asset/scissors.jpg);
+new products ('Shark Sleeping Bag', /asset/shark.jpg);
+new products ('Dust Mop for Babies', /asset/sweep.png);
+new products ('Can of Unicorn Meat', /asset/unicorn.jpg);
+new products ('Robotic Tenticle USB drive', /asset/usb.gif);
+new products ('Reverse Angled Watering Can', /asset/water-cancelAnimationFrame.jpg);
+new products ('Wine Connoisseur\'s Wine Glass', /asset/wine-WebGLShaderPrecisionFormat.jpg);
