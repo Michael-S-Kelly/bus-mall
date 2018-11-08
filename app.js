bef69b90b80b8be9621c45b2c5db3bd300dc47ca
Michael-S-Kelly <mike.s.kelly@icloud.com>
2018-11-07 16:51:04 -0800

    save before I have to leave for appointment

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 'use strict';
 
+//global variables.
+
 var lastSet = [];
 var totCl = 0;
-//create body main
 var mainEL = document.getElementById('mainStuff');
 var div1El = document.createElement('div');
 var titleH1El = document.createElement('h1');
+//create body main
 mainEL.appendChild(titleH1El);
 titleH1El.textContent = 'Please click on your favorite product of the three choices bellow.';
 div1El.id = 'div1';
@@ -26,14 +28,23 @@ thirdImgEl.id = 'thirdImg';
 titleH1El.id = 'request';
 
 
+var products = [];//variable array created to Object data
 
-var products = [];
-
+//create Object Constructor
 function Product(name, catPic) {
   this.name = name; //name of product
   this.catPic = catPic; //href of object
   this.votes = 0; //number of votes received for product
   this.views = 0;
+  
+  
+  var cOne = Math.floor(Math.random() * 255);
+  var cTwo = Math.floor(Math.random() * 255);
+  var cThree =Math.floor(Math.random() * 255);
+  
+  this.bgCol = `rgba(${cOne}, ${cTwo}, ${cThree}, 0.2)`;
+  this.bdrCol = `rgba(${cOne}, ${cTwo}, ${cThree}, 1)`;
+  
   products.push(this);
 }
 
@@ -76,63 +87,93 @@ console.log(products);
 //get image based on index number
 //check to make sure index is unique (doesn't match the last 6 indexes)
 function randImg() {
-  var firstRand = Math.floor(Math.random() * products.length);
-  while (firstRand in lastSet) {
+  
+  var firstRand;
+  var secondRand;
+  var thirdRand;
+  do {
     firstRand = Math.floor(Math.random() * products.length);
-  }
-
-  var secondRand = Math.floor(Math.random() * products.length);
-  while (firstRand in lastSet || secondRand === firstRand) {
     secondRand = Math.floor(Math.random() * products.length);
-  }
-
-  var thirdRand = Math.floor(Math.random() * products.length);
-  while (firstRand in lastSet || thirdRand === firstRand || thirdRand === secondRand) {
     thirdRand = Math.floor(Math.random() * products.length);
-  }
-
-  firstImgEl.src = products[firstRand].catPic;
-  secondImgEl.src = products[secondRand].catPic;
-  thirdImgEl.src = products[thirdRand].catPic;
+  } while (lastSet.includes(firstRand) || lastSet.includes(secondRand) || lastSet.includes(thirdRand) || firstRand === secondRand || firstRand === thirdRand || secondRand === thirdRand);
+  
   lastSet[0] = firstRand;
   lastSet[1] = secondRand;
   lastSet[2] = thirdRand;
   
-  //add ++ to click total
+  console.log(lastSet);
+  
+  firstImgEl.src = products[firstRand].catPic;
+  secondImgEl.src = products[secondRand].catPic;
+  thirdImgEl.src = products[thirdRand].catPic;
+  
+  products[firstRand].views++;
+  products[secondRand].views++;
+  products[thirdRand].views++;
+  
   totCl++;
   
-  //return to generate random index until click total == 25
-  if (totCl === 25) {
-    firstImgEl.removeEventListener('click', randImg);
-    secondImgEl.removeEventListener('click', randImg);
-    thirdImgEl.removeEventListener('click', randImg);
+  if (totCl > 5) {
+    firstImgEl.removeEventListener('click', imgClk);
+    secondImgEl.removeEventListener('click', imgClk);
+    thirdImgEl.removeEventListener('click', imgClk);
     
-    console.log(lastSet[0]);
-    console.log(lastSet[1]);
-    console.log(lastSet[2]);
     results();
   }
-  // this.view[firstRand]++;
-  // this.view[secondRand]++;
-  // this.view[thirdRand]++;
+}
+
+function imgClk(event) {
+  console.log(event.target.alt);
+  for (var i = 0; i < products.length; i++) {
+    if (event.target.alt === products[i].name) {
+      products[i].votes++;
+    }
+  }
+  randImg();
 }
 
 randImg();
 
-function results(){
+function results(){ 
   for (var i = 0; i < products.length; i++) {
-    var listEl = document.createElement('li');
-    listEl.textContent = 'There are ' + products[i].votes + ' votes for the ' + products[i].name + ' and ' + products[i].views + 'views';
-    var resultsEl = document.getElementById('results').getContext('2d');
-    var resultsChart = new RChart(resultsEl, {
+    var resultsEl = document.getElementById('results1');
+    var crtEl = document.createElement('chart').getContext('2d');
+    resultsEl.appendChild(crtEl);
+    crtEl.id = 'chart1';
+    
+    var names;
+    
+    //var listEl = document.createElement('li');
+    //listEl.textContent = 'There are ' + products[i].votes + ' votes for the ' + products[i].name + ' and ' + products[i].views + 'views';
+    
+    for (var j = 0; j < products.length; j++) {
+      names.push(products[i].name);
+    }
+    
+    var votes = [];
+    for (var k = 0; k < products.length; k++) {
+      votes.push(products[j].votes);
+    }
+    
+    var bgClrs =[];
+    for (var l = 0; l < products.length; l++) {
+      bgClrs.push(products[k].bgCol);
+    }
+    
+    var bdrClrs = [];
+    for (var m = 0; m < products.length; m++) {
+      bdrClrs.push(products[k].bdrCol);
+    }
+
+    var rChart = {
       type: 'bar',
       data: {
-        labels: this.name,
+        labels: names,
         datasets: [{
           label: '# of Votes',
-          data: this.votes,
-          backgroundColor: ['Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), 0.2)'],
-          borderColor: 'rgba(255,255,255,1)',
+          data: votes,
+          backgroundColor: bgClrs,
+          borderColor: bdrClrs,
           borderWidth: 1
         }]
       },
@@ -145,12 +186,13 @@ function results(){
           }]
         }
       }
-    });
-    cmEl.appendChild(listEl);
-    mainEL.appendChild(resultsEl);
+    };
+    //cmEl.appendChild(listEl);
+    //mainEL.appendChild(resultsEl);
   }
+  return new Chart(resultsEl, rChart);
 }
 
-firstImgEl.addEventListener('click', randImg);
-secondImgEl.addEventListener('click', randImg);
-thirdImgEl.addEventListener('click', randImg);
+firstImgEl.addEventListener('click', imgClk);
+secondImgEl.addEventListener('click', imgClk);
+thirdImgEl.addEventListener('click', imgClk);
